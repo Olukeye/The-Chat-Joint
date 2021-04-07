1f588d45f7c3bcd0e1dafba261bae698f7d7c858
vevi1 <seunolukeye2000@gmail.com>
2021-04-07 19:55:09 +0100

    enables text to clear off after submit

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,20 +1,41 @@
 const socket = io()
+// elememnts for the Form 
+const $form = document.querySelector('#chat-form')
+const $formInput = $form.querySelector('input')
+const $formButton = $form.querySelector('button')
+
 
 socket.on('message', (message) => {
     console.log(message);
 })
 
 // event for form  to listen for submit
-document.querySelector('#chat-form').addEventListener('submit', (e) => {
+$form.addEventListener('submit', (e) => {
     e.preventDefault()
+    
+    $formButton.setAttribute('disabled', 'disabled')
+    // disabe the form btn
 
-    const message = e.target.elements.message.value
+    const message = e.target.elements.input.value
 
     socket.emit('chat-form', message, (message) => {
+    
+     // enable the form 
+        $formButton.removeAttribute('disabled')
+
+        // clear input text after submit
+        $formInput.value = ''
+        $formInput.focus()
+
+
+
         console.log( message)
+
     })
 })
  
+
+
 // sending location of the client.
 document.querySelector('#map').addEventListener('click',() => {
   if(!navigator.geolocation) {
